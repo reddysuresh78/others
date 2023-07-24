8b83235b849d7f44afa2fdd59380fed14f44c063
Suresh <reddysureshcmc@gmail.com>
2023-07-24 16:43:05 +0530

    Update lines.py

--- lines.py
+++ lines.py
@@ -1,37 +1,17 @@
-const { PDFDocument } = require('pdf-lib');
-const { PDFJS } = require('pdfjs-dist/es5/build/pdf');
+const pdfjsLib = require("pdfjs-dist");
 
-async function extractAndCreatePDF(inputPath, contentToMatch, outputPath) {
-  try {
-    // Read the input PDF
-    const pdfBytes = await PDFJS.getDocument(inputPath).promise;
-    const numPages = pdfBytes.numPages;
-
-    // Create a new PDF
-    const newPDFDoc = await PDFDocument.create();
-
-    for (let pageNum = 1; pageNum <= numPages; pageNum++) {
-      const page = await pdfBytes.getPage(pageNum);
-      const content = await page.getTextContent();
-      const text = content.items.map(item => item.str).join('');
-
-      if (text.includes(contentToMatch)) {
-        const [copiedPage] = await newPDFDoc.copyPages(pdfBytes, [pageNum - 1]);
-        newPDFDoc.addPage(copiedPage);
-      }
-    }
-
-    // Serialize the new PDF to bytes
-    const newPDFBytes = await newPDFDoc.save();
-
-    // Write the new PDF to the output file
-    await fs.writeFile(outputPath, newPDFBytes);
-
-    console.log(`Pages with content "${contentToMatch}" extracted and a new PDF created at: ${outputPath}`);
-  } catch (err) {
-    console.error('Error:', err.message);
-  }
+async function GetTextFromPDF(path) {
+    let doc = await pdfjsLib.getDocument(path).promise;
+    let page1 = await doc.getPage(1);
+    let content = await page1.getTextContent();
+    let strings = content.items.map(function(item) {
+        return item.str;
+    });
+    return strings;
 }
+module.exports = { GetTextFromPDF }
 
-// Usage example
-extractAndCreatePDF('input.pdf', 'keyword to match', 'output.pdf');
+
+
+const pdfExport = require('./pdfreader');
+pdfExport.GetTextFromPDF('/Users/sureshreddy/Documents/Prepared/GIDS_QC_speaker_notes.pdf').then(data => console.log(data));
